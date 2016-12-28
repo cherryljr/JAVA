af2704a3febd679556ae1cc2f01fb85e787b95fa
cherry <cherry>
2016-12-28 22:22:03 +0800

    The digit of Integer part and Decimal part

--- SplitDemo.java
+++ SplitDemo.java
@@ -3,7 +3,7 @@ public class SplitDemo  {
      public static void main(String[] args) {
  				 double number = 234432.234;
  				 String str = String.valueOf(number);
-         String digits[] = str.split("\\.");
+         String digits[] = str.split("\\.");         // split会将.看作一个正则表达式，故前面需要加上转义字符
         
         System.out.println("整数部分位数:" + digits[0].length() + " 小数部分位数:" + digits[1].length());
      }
